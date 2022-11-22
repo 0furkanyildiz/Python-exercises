43c728d6c7e2b28f79e4ccf2018ed5506dc26e42
furkan <furkanyildiz0660@gmail.com>
2022-11-22 23:42:43 +0300

    kucuk duzenleme

--- octadesimal.py
+++ octadesimal.py
@@ -3,17 +3,17 @@ def long(x):
     list1 = []
     if x >= 8:
         while x >= 8:
-            #print(x)
             list1.append(x%8)
             x = x//8
             if x < 8:
                 list1.append(x)
+
         list1.reverse()
         out = "".join(map(str,list1))
         return out
+    
     else:
         return x
-    #print(f"{y} = {str(out)[0]}*8**2+{str(out)[1]}*8**1+{str(out)[2]}*1")
 
 def short(x):
     return str(oct(int(x)))[2:len(str(oct(int(x))))]
